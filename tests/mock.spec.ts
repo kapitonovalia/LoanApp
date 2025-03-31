3c9f2f33dd72a5e0fa7378ff55e40da43dfe96f4
kapitonovalia <aliiakapitonova@gmail.com>
2025-03-31 11:30:43 +0300

    feat|ak|TL-21 prettier

--- tests/mock.spec.ts
+++ tests/mock.spec.ts
@@ -3,23 +3,21 @@ import { SmallLoanPage } from "../page-objects/pages/SmallLoanPage";
 import { LoanDecisionPage } from "../page-objects/pages/LoanDecisionPage";
 
 test.describe("Loan app mock tests", async () => {
-    test("TL-21-1 positive test", async ({ page }) => {
-        const expectedMonthlyAmount = 100005;
-        const smallLoanPage= new SmallLoanPage(page)
+  test("TL-21-1 positive test", async ({ page }) => {
+    const expectedMonthlyAmount = 100005;
+    const smallLoanPage = new SmallLoanPage(page);
 
-     await page.route("**/api/loan-calc*", async (request) => {
-         const responseBody = {paymentAmountMonthly: expectedMonthlyAmount}
-         await request.fulfill({
-             status: 200,
-             contentType: 'application/json',
-             body: JSON.stringify(responseBody)
-         })
-     })
-        const loanCalcResponse = page.waitForResponse("**/api/loan-calc*")
-        await page.goto("http://localhost:3000")
-        await loanCalcResponse;
-        await smallLoanPage.checkMonthlyAmount(expectedMonthlyAmount)
-    })
-
-
-})
+    await page.route("**/api/loan-calc*", async (request) => {
+      const responseBody = { paymentAmountMonthly: expectedMonthlyAmount };
+      await request.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify(responseBody),
+      });
+    });
+    const loanCalcResponse = page.waitForResponse("**/api/loan-calc*");
+    await page.goto("http://localhost:3000");
+    await loanCalcResponse;
+    await smallLoanPage.checkMonthlyAmount(expectedMonthlyAmount);
+  });
+});
